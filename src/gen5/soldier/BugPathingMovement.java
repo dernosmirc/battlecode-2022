6ab11730cbee645892b9352395ea17e976ea9dc5
Himanshu <himanshukumar1125@gmail.com>
2022-01-18 18:14:44 +0530

    Added move code to BugPathingMovement

--- src/gen5/soldier/BugPathingMovement.java
+++ src/gen5/soldier/BugPathingMovement.java
@@ -4,12 +4,14 @@ import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotType;
+import battlecode.common.RobotInfo;
 import gen5.common.BugPathingHelper;
 import gen5.common.CommsHelper;
 import gen5.common.MovementHelper;
 
 import static gen5.RobotPlayer.*;
 import static gen5.Soldier.*;
+import static gen5.common.Functions.getDistance;
 
 public class BugPathingMovement {
     private static int INNER_DEFENSE_RADIUS = 4;
@@ -17,29 +19,51 @@ public class BugPathingMovement {
     private static int INNER_ATTACK_RADIUS = 8;
     private static int OUTER_ATTACK_RADIUS = 13;
 
+    private static final int HEAL_THRESHOLD = 20; // 21
+    private static final int FULL_HEAL_THRESHOLD = 45;
+
     public static void move() throws GameActionException {
         MapLocation defenseLocation = DefenseHelper.getDefenseLocation();
         if (defenseLocation != null) {
-            int distance = rc.getLocation().distanceSquaredTo(defenseLocation);
-            if (distance < INNER_DEFENSE_RADIUS) {
-                BugPathingHelper.setDefault();
-                Direction dir = rc.getLocation().directionTo(defenseLocation).opposite();
-                MovementHelper.greedyTryMove(dir);
-            } else if (INNER_DEFENSE_RADIUS <= distance && distance <= OUTER_DEFENSE_RADIUS) {
-                BugPathingHelper.setDefault();
-                DefenseHelper.tryMoveRight(defenseLocation);
-            } else if (distance <= RobotType.ARCHON.visionRadiusSquared) {
-                BugPathingHelper.setDefault();
-                Direction dir = rc.getLocation().directionTo(defenseLocation);
-                if (MovementHelper.greedyTryMove(dir)) {
-                    return;
-                } else {
-                    DefenseHelper.tryMoveRightAndBack(dir);
+            circleAround(defenseLocation);
+            return;
+        }
+
+        RobotInfo[] robots = rc.senseNearbyRobots(myType.actionRadiusSquared, myTeam); // try vision radius?
+        RobotInfo archon = null;
+        for (int i = robots.length; --i >= 0; ) {
+            RobotInfo robot = robots[i];
+            if (robot.type == RobotType.ARCHON) {
+                archon = robot;
+                break;
+            }
+        }
+
+        if (archon != null && rc.getHealth() < FULL_HEAL_THRESHOLD) {
+            circleAround(archon.location);
+            return;
+        }
+
+        if (rc.getHealth() <= HEAL_THRESHOLD) {
+            MapLocation[] archons = CommsHelper.getFriendlyArchonLocations();
+            int minDistance = rc.getMapWidth() * rc.getMapHeight();
+            MapLocation archonLocation = null;
+            for (int i = maxArchonCount; --i >= 0; ) {
+                if (archons[i] != null) {
+                    // if (i == myArchonIndex) {
+                    //     archonLocation = archons[i];
+                    //     break;
+                    // }
+
+                    int distance = getDistance(archons[i], rc.getLocation());
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        archonLocation = archons[i];
+                    }
                 }
-            } else {
-                BugPathingHelper.moveTowards(defenseLocation);
             }
 
+            circleAround(archonLocation);
             return;
         }
 
@@ -75,5 +99,27 @@ public class BugPathingMovement {
         }
 
         BugPathingHelper.moveTowards(guessedEnemyArchonLocation);
+    }
+
+    private static void circleAround(MapLocation location) throws GameActionException {
+        int distance = rc.getLocation().distanceSquaredTo(location);
+        if (distance < INNER_DEFENSE_RADIUS) {
+            BugPathingHelper.setDefault();
+            Direction dir = rc.getLocation().directionTo(location).opposite();
+            MovementHelper.greedyTryMove(dir);
+        } else if (INNER_DEFENSE_RADIUS <= distance && distance <= OUTER_DEFENSE_RADIUS) {
+            BugPathingHelper.setDefault();
+            DefenseHelper.tryMoveRight(location);
+        } else if (distance <= RobotType.ARCHON.visionRadiusSquared) {
+            BugPathingHelper.setDefault();
+            Direction dir = rc.getLocation().directionTo(location);
+            if (MovementHelper.greedyTryMove(dir)) {
+                return;
+            } else {
+                DefenseHelper.tryMoveRightAndBack(dir);
+            }
+        } else {
+            BugPathingHelper.moveTowards(location);
+        }
     }
 }
